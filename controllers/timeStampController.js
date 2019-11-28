f15584b5acff568a2dd543faface932e873e55b2
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2019-11-28 15:32:36 -0600

    Bug Fixes - Fixed bug on glitch.me with no req params on api endpoint the response was null

--- controllers/timeStampController.js
+++ controllers/timeStampController.js
@@ -1,12 +1,12 @@
-// TODO AR: design controller
 const helpers = require('../helpers');
 
 exports.getStamp = (req, res) => {
     let { date_string } = req.params;
     if (date_string == undefined) {
         date_string = new Date();
+    } else if ( date_string.length > 0) {
+        date_string = helpers.convertDate(date_string);
     }
-    date_string = helpers.convertDate(date_string);
     const date = new Date(date_string);
     const payload = {
         unix: date.getTime(),
