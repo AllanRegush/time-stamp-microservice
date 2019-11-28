e65b58bed8f6733c0a533e0757984de30acdcdc8
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2019-11-28 16:22:31 -0600

    Added Cors

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const routes = require('./routes/index')
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(cors({optionSuccessStatus: 200}))
 
 app.use('/', routes);
 
